51c3147d3e6c884bf25adb62d2884bf8922a08e8
mapTOdotCA <mapTOdotCA@gmail.com>
2020-01-05 16:46:22 -0500

    init

---
+++ config.js
@@ -0,0 +1,131 @@
+// Number of slides that will drive (more = smoother)
+// If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
+var driveSlides = 7;
+
+// Number of points on drive route (more = higher quality, but slower to process)
+var driveSmoothness = 200;
+
+// Value used to drive
+var driveTime = driveSlides*driveSmoothness;
+
+// Do you want to follow the point? True = follow
+var followPoint = true;
+
+// ...If so, what zoom, pitch, and bearing should be used to follow?
+var followZoomLevel = 14;
+var followBearing = 29;
+var followPitch = 0;
+
+// to add 'driving' slides just make sure to add 'drive to beginning of slide id'
+// you also need to add a running total to the end of each 'drive-slide', (ex. drive-slide-0, drive-slide-1, drive-slide-2, etc.)
+var config = {
+    style: 'mapbox://styles/mapbox/light-v10',
+    // style: 'mapbox://styles/mapbox/satellite-v8',
+    // style: 'mapbox://styles/mapbox/outdoors-v10',
+    accessToken: 'pk.eyJ1Ijoid2lsbGlhbWJlbmRhdmlzIiwiYSI6IlVrb3BGVzQifQ.jeHxDCnpXXvAXKfAFEYG-A',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'left',
+    title: 'The Title Text of this Story',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By a Digital Storyteller',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: 'slide-0',
+            title: 'Display Title',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-0',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                // location information is from the drive route
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-1',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-2',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-3',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-4',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+        // You can add a normal slide in the middle of the drive slides to take a break
+        // {
+        //     id: 'slide-4',
+        //     title: 'Second Title',
+        //     image: './path/to/image/source.png',
+        //     description: 'Copy these sections to add to your story.',
+        //     location: {
+        //     },
+        //     onChapterEnter: [],
+        //     onChapterExit: []
+        // },
+        {
+            id: 'drive-slide-5',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-6',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'slide-2',
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {},
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
---
+++ js/drive.js
@@ -0,0 +1,93 @@
+var routeData = [];
+
+var geojsonPoint = {
+    "type": "FeatureCollection",
+    "features": [{
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+        
+            ]
+        }
+    }]
+};
+
+function createLine() {
+
+    // get the coordinates of the line you want to highlight
+    let extentArray = routeData.features[0].geometry.coordinates;
+    
+    // create a turf linestring based on the line coordinates
+    const line = turf.lineString(extentArray);
+
+    // calculate the total length of the line
+    const lineDistance = turf.lineDistance(line);
+
+    // how many points you want along the path (more = smoother animation)
+    const rects = driveTime;
+
+    // calculate the distance between each point on the path
+    const segments = lineDistance / rects;
+
+    // what units do you want to use?
+    const units = 'kilometers';
+
+    // based on the number of points...
+    for(let i = 0; i <= rects; i++) {
+
+        // calculate point location for each segment
+        const pointonline = turf.along(line, (segments * i));
+
+        // push new x,y
+        let newX = pointonline.geometry.coordinates[0];
+        let newY = pointonline.geometry.coordinates[1];
+
+        geojsonPoint.features[0].geometry.coordinates.push([newX, newY]);
+
+        // draw our initinal point
+        if (i === 0) {
+            let initPoint = turf.point([newX, newY]);
+
+            // if you want to follow the point...
+            if (followPoint === true) {
+            	map.setCenter([newX, newY]);
+        	}
+
+            map.getSource('pointSource').setData(initPoint);
+        }
+
+        // once 'i' equals the number of points then we're done building our line 
+        if (i == rects) {
+            map.getSource('lineSource').setData(geojsonPoint);
+        }
+    }
+
+}
+
+function changeCenter(index) {
+    let currentJson = geojsonPoint.features[0].geometry.coordinates.slice(0,index);
+    let center = geojsonPoint.features[0].geometry.coordinates[index];
+    let centerX = center[0];
+    let centerY = center[1];
+    let movingLine = {
+        "type": "FeatureCollection",
+        "features": [{
+            "type": "Feature",
+            "geometry": {
+                "type": "LineString",
+                "coordinates": currentJson
+            }
+        }]
+    };
+    let movingPoint = turf.point([centerX, centerY]);
+    map.getSource('lineSource').setData(movingLine);
+    map.getSource('pointSource').setData(movingPoint);
+
+    // if you want to follow the point...
+    if (followPoint === true) {
+	    map.jumpTo({
+	        center: [centerX, centerY]
+	    });
+    }
+}
